742027e30b3c66bd83b2f728f50a0c340607ca01
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2023-03-13 15:03:38 +0000

    added xgboost and rf

--- project_adv_ml/models.py
+++ project_adv_ml/models.py
@@ -1,10 +1,12 @@
-from darts.models import NBEATSModel
+from darts.models import NBEATSModel, RandomForest
 from darts.metrics import rmse
 from darts.utils.likelihood_models import QuantileRegression
 import numpy as np
 from skopt import gp_minimize
 from skopt.space import Integer
 from skopt.utils import use_named_args
+
+# TODO change following into K-fold cross validation if time permits
 
 class HyperparameterOptimizationNBEATS:
     """
@@ -108,6 +110,100 @@ class HyperparameterOptimizationNBEATS:
             """
             params = (num_stacks, num_blocks, layer_width, n_epochs, nr_epochs_val_period, input_length)
             return self.objective_nbeats(params)
+        
+        result = gp_minimize(objective, self.space, n_calls=n_calls)
+        return result
+
+class HyperparameterOptimizationRandomForest:
+    """
+    Class for hyperparameter optimization of a Random Forest Model
+    using k fold cross validation and Bayesian optimization.
+
+    Args:
+        train_target (TimeSeries): Training target
+        train_past_cov (TimeSeries): Training past covariates
+        val_target_input (TimeSeries): Validation target
+        val_past_cov (TimeSeries): Validation past covariates
+        seed (int, optional): Random seed. Defaults to 42.
+    """
+    def __init__(self, output_length, train_target, train_future_cov, val_target, val_input, val_future_cov, seed=42):
+        assert isinstance(seed, int), "seed must be of type int"
+        self.output_length = output_length
+        self.train_target = train_target
+        self.train_future_cov = train_future_cov
+        self.val_target = val_target
+        self.val_future_cov = val_future_cov
+        self.val_input = val_input,
+        self.seed = seed
+        self.space = [
+            Integer(2, 10, name='max_depth'),
+            Integer(2, 10, name='min_samples_split'),
+            Integer(1, 5, name='min_samples_leaf'),
+            Integer(10, 500, name='n_estimators'),
+            Integer(1, 3, name='n_jobs'),
+            Integer(3, 12, name='lags'),
+            Integer(-12, -1, name='max_lag_future_cov')]
+    
+    def objective_rf(self, params):
+        """ 
+        Optimization function for hyperparameter tuning
+
+        Args:
+            params (tuple): hyperparameters to optimize
+            train_target (TimeSeries): training target
+            train_past_cov (TimeSeries): training past covariates
+            test_target (TimeSeries): validation target
+            test_past_cov (TimeSeries): validation past covariates
+
+        Returns:
+            float: validation loss
+        """
+        max_depth, min_samples_split, min_samples_leaf, n_estimators, n_jobs, lags, max_lag = map(int, params)
+        model = RandomForest(   lags=lags,
+                                lags_future_covariates=[k for k in range(max_lag, 1)],
+                                max_depth=max_depth,
+                                min_samples_split=min_samples_split,
+                                min_samples_leaf=min_samples_leaf,
+                                n_estimators=n_estimators,
+                                n_jobs=n_jobs,
+                                random_state=self.seed)
+        model.fit(series=self.train_target, 
+                  future_covariates=self.train_future_cov)
+        preds_val = model.predict(n=self.output_length, 
+                                  series=self.val_input,
+                                  future_covariates=self.val_future_cov)
+        return np.mean([rmse(target[-self.output_length:], pred) for target, pred in zip(self.val_target, preds_val)])
+
+    def optimize(self, n_calls=50):
+        """
+        Optimize hyperparameters using Bayesian optimization and Gaussian processes.
+
+        Args:
+            n_calls (int, optional): Number of calls to the objective function. Defaults to 50.
+        
+        Returns:
+            skopt.OptimizeResult: Optimization result
+        """
+        # Wrap objective function to use named arguments
+        @use_named_args(self.space)
+        
+        def objective(max_depth, min_samples_split, min_samples_leaf, n_estimators, n_jobs, lags):
+            """
+            Objective function for hyperparameter tuning
+
+            Args:
+                max_depth (int): Maximum depth of the tree
+                min_samples_split (int): Minimum number of samples required to split an internal node
+                min_samples_leaf (int): Minimum number of samples required to be at a leaf node
+                n_estimators (int): Number of trees in the forest
+                n_jobs (int): Number of jobs to run in parallel
+                lags (int): Number of lags
+            
+            Returns:
+                float: validation loss
+            """
+            params = (max_depth, min_samples_split, min_samples_leaf, n_estimators, n_jobs, lags)
+            return self.objective_rf(params)
         
         result = gp_minimize(objective, self.space, n_calls=n_calls)
         return result
